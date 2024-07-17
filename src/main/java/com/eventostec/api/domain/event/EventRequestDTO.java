8ccd9c4e810fd1c280c656e75c2f5f7061298c8e
Matheus Gonçalves Rodrigues <mgrbq@outlook.com>
2024-07-17 18:43:10 -0300

    created event service

--- src/main/java/com/eventostec/api/domain/event/EventRequestDTO.java
+++ src/main/java/com/eventostec/api/domain/event/EventRequestDTO.java
@@ -1,4 +1,18 @@
 package com.eventostec.api.domain.event;
 
-public class EventRequestDTO {
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Date;
+
+public record EventRequestDTO(
+        String title,
+        String description,
+        Long date,
+        String city,
+        String state,
+        Boolean remote,
+        String eventUrl,
+        MultipartFile image
+)
+{
 }
